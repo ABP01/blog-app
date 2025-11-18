f48c078f59dc560a5e6e3592d0a3578d10875dae
tgede46 <gedeonkpara@gmail.com>
2025-11-18 11:55:47 +0000

    Add authentication and error handling features; implement remote data source and repository

---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,5 @@
+class AppSecrets {
+  static const supabaseUrl = 'https://adyrmuucxdosipzekxaa.supabase.co';
+  static const supabaseAnonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFkeXJtdXVjeGRvc2lwemVreGFhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM0NTkyMzAsImV4cCI6MjA3OTAzNTIzMH0.r5qJX_g0RUeRlggrdSjRSOZpfDBdUajdPtjtAZ65Clc';
+}
